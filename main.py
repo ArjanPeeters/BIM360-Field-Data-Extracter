52e8374a9845583d4e34d805ca1c29483746e20f
ArjanPeeters <arjan.peeters@gmail.com>
2018-08-07 15:15:26 +0200

    Added choise for server location and a logger
    Added choise for server location and comments in the .ini file
    Added a log file as output file. for debugging puposes

--- main.py
+++ main.py
@@ -3,30 +3,67 @@ import requests
 import json
 import datetime
 import os
+import ssl
+import logging
+print(ssl.OPENSSL_VERSION)
 try:
-    from ConfigParser import SafeConfigParser
+  from ConfigParser import SafeConfigParser
 except ImportError:
-    from configparser import SafeConfigParser
-
+  from configparser import SafeConfigParser
 
 # get info from config.ini
 config = SafeConfigParser()
-config.read('config.ini')
+try:
+  config.read('config.ini')
+except Exception:
+  print("error reading config.ini")
 
-username = config.get("Field", "username")
-password = config.get("Field", "password")
-client_id = config.get("HQ", "client_id")
-client_secret = config.get("HQ", "client_secret")
-account_id = config.get("HQ", "account_id")
-
+timestamp = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
 save_path = config.get("paths", "main_path")
 backup_path = config.get("paths", "backup_path")
 if not os.path.isdir(save_path):
-  print("Created directory", save_path)
+  logger.debug("Created directory: {}".format(save_path))
   os.makedirs(backup_path)
+backup_path_ts = os.path.join(backup_path,timestamp)
+if not os.path.isdir(backup_path_ts):
+  os.makedirs(backup_path_ts)
+
+logging.basicConfig(filename='{bpt}/_harvest_{ts}.log'.format(bpt=backup_path_ts,ts=timestamp),level=logging.INFO,filemode='w',format='%(asctime)s [%(levelname)-12.12s] %(message)s',datefmt='%Y-%m-%d,%H:%M:%S')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.formatter('%(asctime)s -> %(message)s',datefmt='%Y-%m-%d,%H:%M:%S')
+fh = logging.FileHandler('{bpt}/harvest_{ts}.log'.format(bpt=backup_path_ts,ts=timestamp))
+fh.setLevel(logging.DEBUG)
+fh.formatter('%(asctime)s [%(levelname)-8.8s] %(message)s',datefmt='%Y-%m-%d,%H:%M:%S')
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+username = config.get("Field", "username")
+logger.debug('username={}'.format(username))
+password = config.get("Field", "password")
+logger.debug('password={0}{1}'.format(password[:1],(len(password)-1)*"*"))
+client_id = config.get("HQ", "client_id")
+logger.debug('client id={}'.format(client_id))
+client_secret = config.get("HQ", "client_secret")
+logger.debug('client_secret={0}{1}'.format(client_secret[:1],(len(client_secret)-1)*"*"))
+account_id = config.get("HQ", "account_id")
+logger.debug('account_id={}'.format(account_id))
+
+if config.get("Server", "location") == "US":
+  base_url_field = "https://bim360field.autodesk.com/"
+  base_url_hub = "https://developer.api.autodesk.com/hq/v1/accounts/{0}".format(account_id)
+else:
+  base_url_field = "https://bim360field.{0}.autodesk.com/".format(config.get("Server", "location"))
+  base_url_hub = "https://developer.api.autodesk.com/hq/v1/regions/{1}/accounts/{0}".format(account_id,config.get("Server", "location"))
+logger.debug('base_url_hub={}'.format(base_url_hub))
+logger.debug('base_url_field={}'.format(base_url_field))
+
+
+
 
 # save file function
-timestamp = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
 def double_save_file(name, data):
   """
   Checks if directories excist and makes them if not
@@ -41,24 +78,21 @@ def double_save_file(name, data):
   backup_file = os.path.join(backup_path_ts,backupname)
   with open(save_file, 'w') as outfile:
     json.dump(data, outfile)
-  print('{fn} saved'.format(fn=filename))
+  logger.info('{fn} saved'.format(fn=filename))
   with open(backup_file, 'w') as outbackup:
     json.dump(data, outbackup)
-  print('{fn} saved'.format(fn=backupname))
-
-
-BASE_URL_FIELD = "https://bim360field.eu.autodesk.com/"
-BASE_URL_EU_HUB = "https://developer.api.autodesk.com/hq/v1/regions/eu/accounts/{0}".format(account_id)
+  logger.info('{fn} saved'.format(fn=backupname))
 
 # ignore projects in this list
 ignore_projects = config.get('ignore_projects','name')
+logger.info('ignoring projects: '.format(ignore_projects))
 
 # control what info you want to download
 harvest_info = {}
-print("Downloading:")
+logger.info("Downloading:")
 for name in config.options('download'):
   harvest_info[name] = config.getboolean('download',name)
-  print("{:20}:{}".format(name,config.getboolean('download',name)))
+  logger.info("{:20}:{}".format(name,config.getboolean('download',name)))
 
 
 # function for FIELD commands
@@ -74,7 +108,7 @@ def field_api_cmd(soort, command, payload):
     #if not logedin yet, login
     field_ticket = json.loads(
       requests.request(
-        "POST","https://bim360field.eu.autodesk.com/api/login",
+        "POST","{0}/api/login".format(base_url_field),
         data = {
           "username": username,
           "password": password
@@ -83,18 +117,25 @@ def field_api_cmd(soort, command, payload):
           'content-type': "application/x-www-form-urlencoded",
           'Cache-Control': "no-cache"
         }).text)['ticket']
-    print ('Field ticket: {0}'.format(field_ticket))
+    logger.debug("Logging into {0}".format(base_url_field))
+    logger.info('Field ticket: {0}'.format(field_ticket))
 
-  url = '{base_url}{command}'.format(base_url=BASE_URL_FIELD, command=command)
+  url = '{base_url}{command}'.format(base_url=base_url_field, command=command)
 
   headers = {
-      'content-type': "application/x-www-form-urlencoded",
-      'Cache-Control': "no-cache",
+    'content-type': "application/x-www-form-urlencoded",
+    'Cache-Control': "no-cache",
   }
 
   payload['ticket'] = field_ticket
 
+  logger.debug("FIELD Request= soort:{soort}, url:{url}".format(soort=soort,url=url))
+  logger.debug("data:{}".format(payload))
+  logger.debug("headers:{}".format(headers))
+
   response = requests.request(soort, url, data=payload, headers=headers).json()
+
+  logger.debug("FIELD returned: {} records".format(len(response)))
   return response
 
 
@@ -126,7 +167,7 @@ def bim360_api_cmd(soort, command, **payload):
           'Content-Type': "application/x-www-form-urlencoded",
           'Cache-Control': "no-cache"
         }).text)['access_token']
-    print("Hub login succesfull: {0}...{1}".format(access_token[:5],access_token[-5:]))
+    logger.info("Hub login succesfull: {0}...{1}".format(access_token[:5],access_token[-5:]))
 
   payload['scope'] = "account:read"
   payload['grant_type'] = 'client_credentials'
@@ -134,13 +175,20 @@ def bim360_api_cmd(soort, command, **payload):
   headers['Content-Type'] = "application"
   headers['Authorization'] = "Bearer {0}".format(access_token)
 
-  _url = '{base_url}/{command}'.format(base_url=BASE_URL_EU_HUB, command=command)
+  _url = '{base_url}/{command}'.format(base_url=base_url_hub, command=command)
+
+  logger.debug("HQ Request= soort:{soort}, url:{url}".format(soort=soort,url=_url))
+  logger.debug("params:{}".format(payload))
+  logger.debug("headers:{}".format(headers))
 
   response = requests.request(
     soort,
     _url,
     params=payload,
     headers=headers).json()
+
+  logger.debug("HQ returned: {} records".format(len(response)))
+
   return response
 
 
@@ -153,21 +201,21 @@ all_project_names = []
 get_projects = field_api_cmd("GET", "api/projects",{})
 
 # extract project id's and hq id's
-for i in range(len(get_projects)):
-  current_project = get_projects[i]
-  if current_project['name'] not in ignore_projects:
-    all_project_hqs.append(current_project['hq_identifier'])
-    all_project_ids.append(current_project['project_id'])
-    all_project_names.append(current_project['name'])
+for i in get_projects:
+  if i['name'] not in ignore_projects:
+    all_project_hqs.append(i['hq_identifier'])
+    all_project_ids.append(i['project_id'])
+    all_project_names.append(i['name'])
+    logger.debug("Adding project, name: {0}, ID: {1}, HQ: {2}".format(i['name'],i['project_id'],i['hq_identifier']))
 
-print(len(all_project_ids), " projects are being harvested")
+logger.info("{} projects are harvested".format(len(all_project_ids)))
 
 # get all project info from HQ
 if harvest_info['project_info']:
   all_project_info = []
   for i, project_id in enumerate(all_project_hqs):
     all_project_info.append(bim360_api_cmd('GET','projects/{hq}'.format(hq=project_id)))
-    print('adding project info from {name}'.format(name=all_project_info[i]['name']))
+    logger.info('adding project info from {name}'.format(name=all_project_info[i]['name']))
     all_project_info[i]['field_project_id'] = all_project_ids[i]
   double_save_file('Project_info', all_project_info)
 
@@ -182,10 +230,10 @@ def get_standard_field_records(type_name, type_query, api_location, **parameters
     if records:
       for record in records:
         record['project_id'] = current_project_id
-      print('Adding {} {} for project: {}'.format(len(records), type_name, all_project_names[i]))
+      logger.info('Adding {} {} for project: {}'.format(len(records), type_name, all_project_names[i]))
       all_records.append(records)
     else:
-      print('{} {} for project: {}'.format(len(records), type_name, all_project_names[i]))
+      logger.info('{} {} for project: {}'.format(len(records), type_name, all_project_names[i]))
   double_save_file("{}_info".format(type_name), all_records)
 
 
@@ -201,7 +249,7 @@ def get_standard_hq_records(type_name, type_query, api_location, limitandrespons
       all_records.append(get_records[i])
     offset += limit
     response = len(get_records)
-  print('Adding {} {}'.format(len(all_records),type_name))
+  logger.info('Adding {} {}'.format(len(all_records),type_name))
   double_save_file('{}_info'.format(type_name), all_records)
 
 # get contacts from field
@@ -260,7 +308,7 @@ if harvest_info['categories']:
     for equipment_sets_category in equipment_sets_categories:
         equipment_sets_categories['project_id'] = current_project_id
 
-    print(
+    logger.info(
         "Adding {0} Normal cat, {1} Custom Cat and {2} Equipment Status"
         .format(len(normal_categories), len(custom_categories), len(equipment_sets_categories))
     )
@@ -274,4 +322,4 @@ if harvest_info['categories']:
   double_save_file("Categories_Equipment_sets_info", all_categories_equipment_sets)
 
 
-print("- - Done - -")
+logger.info("- - Done - -")
